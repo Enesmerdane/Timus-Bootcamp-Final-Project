1c6313c419345b1d1c4d20f95f8a5662a6e358dd
enesmerdane <ensmerdane@gmail.com>
2023-12-11 13:10:40 +0300

    elasticsearch connection added

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ElasticDbModule } from './elastic-db/elastic-db.module';
 
 @Module({
-  imports: [],
+  imports: [ElasticDbModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -1,8 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, HttpException } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
+  constructor(@Inject('ELASTICSEARCH_CONNECTION') private elasticConn: any) {}
+
+  async getHello() {
+    // await this.elasticConn
+    //   .index({
+    //     index: '123123',
+    //     document: {
+    //       title: 'gelisims',
+    //       year: 1882,
+    //     },
+    //   })
+    //   .then((val) => {
+    //     console.log('it worked');
+    //     console.log(val);
+    //   })
+    //   .catch((err) => {
+    //     throw new HttpException(
+    //       {
+    //         status: 'error',
+    //         message: 'Unable to reach ElasticSearch cluster: ' + err,
+    //       },
+    //       500,
+    //     );
+    //   });
     return 'Hello World!';
   }
 }
