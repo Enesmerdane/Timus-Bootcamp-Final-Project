05ab4dcb4621c34da075e8d01d85ac3a0604e051
enesmerdane <ensmerdane@gmail.com>
2023-12-13 11:54:53 +0300

    BE - added express rate limit

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,6 +4,7 @@ import * as cookieParser from 'cookie-parser';
 import { ValidationPipe } from '@nestjs/common';
 import { useContainer } from 'class-validator';
 const { xss } = require('express-xss-sanitizer');
+import { rateLimit } from 'express-rate-limit';
 
 require('dotenv').config();
 
@@ -11,7 +12,18 @@ async function bootstrap() {
     // Initiate Nest server
     const app = await NestFactory.create(AppModule);
 
-    app.use(xss());
+    app.use(xss()); // express xss sanitizer
+
+    const limiter = rateLimit({
+        windowMs: 15 * 60 * 1000, // 15 minutes
+        limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+        standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+        legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+        // store: ... , // Use an external store for consistency across multiple server instances.
+    });
+
+    // Apply the rate limiting middleware to all requests.
+    app.use(limiter);
 
     app.useGlobalPipes(new ValidationPipe());
 
