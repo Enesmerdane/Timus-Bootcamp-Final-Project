2a2f070186c9f3d82e37039b8d4bd10c685be6d9
enesmerdane <ensmerdane@gmail.com>
2023-12-16 13:01:01 +0300

    vuetify deneme

--- client/src/main.ts
+++ client/src/main.ts
@@ -6,11 +6,28 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
 
 const app = createApp(App)
 
+const vuetify = createVuetify({
+    components,
+    directives,
+    icons: {
+        defaultSet: 'mdi',
+        aliases,
+        sets: {
+            mdi
+        }
+    }
+})
+
+app.use(vuetify)
 app.use(createPinia())
 app.use(router)
 
